da67de58577aaed9f108bceb3fc331982801c576
arzutr <52047628+arzutr@users.noreply.github.com>
2021-06-18 23:39:23 +0300

    Create MIRTConstantReplacer.java

---
+++ muPLSQL/src/org/muplsql/transform/sql/MIRTConstantReplacer.java
@@ -0,0 +1,68 @@
+package org.muplsql_full.transform.sql;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.muplsql_full.mcase.Case;
+import org.muplsql_full.transform.MITransformer;
+import org.muplsql_full.transform.MutaOperatorType;
+
+public class MIRTConstantReplacer  extends MITransformer {
+
+
+	public MIRTConstantReplacer() {
+		this.mtype = MutaOperatorType.Mot34.name();
+		this.mopId = 34;oCcase = new Case();
+	}
+
+
+	@Override
+	public List<String> mutateAsList(String value) {
+		List<String> result = new ArrayList<>();
+
+
+
+
+		if (value.equalsIgnoreCase("'") && oCcase.tStarted == 1  &&  checkPreCondition(null,null)){
+			oCcase.tStarted=0;
+			if (value.equalsIgnoreCase("NULL") && hasNotNot()) {
+				result.add("xyz'"  );
+			}
+		}
+		if (value.equalsIgnoreCase("'")  && oCcase.tStarted == 0)
+			oCcase.tStarted++;
+		
+		if (value.equalsIgnoreCase(";")  && checkPreCondition(null,null)){
+			oCcase.reset();
+		}
+		if (value.equalsIgnoreCase("where") )
+			oCcase.whereStarted++;
+		if (value.equalsIgnoreCase("select") )
+			oCcase.selectStarted++;		
+		return result;
+	}
+
+	private boolean hasNotNot() {
+		return oCcase.preString== null || !oCcase.preString.equalsIgnoreCase("not");
+	}
+
+	public static void main(String[] a) {
+
+	}
+
+	@Override
+	public boolean checkPreCondition(String value, String valueNext) {
+		return oCcase.whereStarted==0 || oCcase.selectStarted ==0;
+		//return value.equalsIgnoreCase(valueNext);
+	}
+	
+	@Override
+	public Case getCase() {
+		return this.oCcase;
+	}
+
+	@Override
+	public  boolean hasPreCondition(){
+		return true;
+	}
+}
