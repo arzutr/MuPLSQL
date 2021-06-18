0fa7990053f21f015af1e139afdc82c24efb4744
arzutr <52047628+arzutr@users.noreply.github.com>
2021-06-18 23:45:26 +0300

    Update MExceptionInsertion.java

--- muPLSQL/src/org/muplsql/transform/struct/MExceptionInsertion.java
+++ muPLSQL/src/org/muplsql/transform/struct/MExceptionInsertion.java
@@ -17,8 +17,17 @@ public class MExceptionInsertion  extends MITransformer {
 	public List<String> mutateAsList(String value) {
 		List<String> result = new ArrayList<>();
 		//NoteL once digeri icin calistirildi
+	
 		if (value.equalsIgnoreCase("OTHERS"))
 			result.add("TOO_MANY_ROWS"  );
+		if (value.equalsIgnoreCase("TOO_MANY_ROWS"))
+			result.add("OTHERS"  );
+		
+		if (value.equalsIgnoreCase("NO_DATA_FOUND"))
+			result.add("OTHERS"  );
+		
+		if (value.equalsIgnoreCase("OTHERS"))
+			result.add("NO_DATA_FOUND"  );
 
 		return result;
 	}
