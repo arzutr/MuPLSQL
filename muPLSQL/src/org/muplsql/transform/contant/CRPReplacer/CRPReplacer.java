caceb148c6dc2802a8ebe184ee8db0894593f9f6
arzutr <52047628+arzutr@users.noreply.github.com>
2021-06-18 23:29:16 +0300

    Create CRPReplacer.java

---
+++ muPLSQL/src/org/muplsql/transform/contant/CRPReplacer/CRPReplacer.java
@@ -0,0 +1,36 @@
+package org.muplsql_full.transform.constant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.muplsql_full.transform.MITransformer;
+import org.muplsql_full.transform.MutaOperatorType;
+
+public class CRPReplacer extends MITransformer {
+
+	public CRPReplacer() {
+		this.mtype = MutaOperatorType.Mot68.name();
+		this.mopId = 68;
+	}
+
+	@Override
+	public List<String> mutateAsList(String value) {
+		List<String> result = new ArrayList<>();
+
+		if (value.equals("'")){
+			result.add("xxx'");
+		}
+		///begin den once is den sonra olan '' constant;lar
+		return result;
+	}
+	
+//SQL dekindeyse like olmayanlar dier durumda like bozulur.
+	//hic degismeyen olmasi kosulu var.
+// precondition eklenebilir ilerde
+	//:= ve ' icersin 
+			
+	public static void main(String[] a) {
+
+	}
+
+}
