7913c8bab58ad4f2701bf34d51011dfb9648a4a0
arzutr <52047628+arzutr@users.noreply.github.com>
2021-06-18 23:22:35 +0300

    Create ConstantArrayReferenceReplacer.java

---
+++ muPLSQL/src/org/muplsql/transform/array/ConstantArrayReferenceReplacer.java
@@ -0,0 +1,75 @@
+package org.muplsql_full.transform.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.muplsql_full.transform.MITransformer;
+import org.muplsql_full.transform.MutaOperatorType;
+
+public class ConstantArrayReferenceReplacer  extends MITransformer {
+
+	public ConstantArrayReferenceReplacer() {
+		this.mtype = MutaOperatorType.Mot82.name();
+		this.mopId = 82;
+	}
+  @Override
+			public List<String> mutateAsList(String value){
+				if (true)
+					throw new RuntimeException("This mutation operator works with code analysis");
+			
+				return null;
+			}
+
+			public List<String> mutateAsList(String value,  String changeList[][]) {
+				List<String> result = new ArrayList<>();
+
+				if (value.equalsIgnoreCase(";") && 
+						oCcase.whereStarted == 0  && 
+						oCcase.ifStarted  	== 0 && 
+						oCcase.assignStarted == 1) {
+
+					for (int i = 0; i < changeList.length; i++) {
+
+							if (oCcase.preString.contains(changeList[i][0])) {
+								result.add(changeList[i][0] + ";");
+							}
+						
+					}
+
+				}
+				
+				if (oCcase.whereStarted  == 0 && oCcase.ifStarted  == 0){
+					oCcase.reset();
+				}
+
+				if (value.equalsIgnoreCase("where"))
+					oCcase.whereStarted++;
+
+				if (value.equalsIgnoreCase("if") )
+					oCcase.ifStarted++;
+				
+				if (value.equalsIgnoreCase(":=") )
+					oCcase.assignStarted++;
+				
+				if (value.equalsIgnoreCase(";") )
+					oCcase.reset();
+				
+				return result;
+			}
+			
+			@Override
+			public  boolean hasPreCondition(){
+				return false;
+			}
+			
+			@Override
+			public boolean checkPreCondition(String value, String valueNext) {
+				return (oCcase.whereStarted == 0); // not in where clauses
+
+			}
+
+			
+			public String getMType(){
+				return "DB";
+			}
+	}
