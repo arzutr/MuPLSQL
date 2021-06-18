1c29d3d674c49b8c8716b99b24fe11bd6908453b
arzutr <52047628+arzutr@users.noreply.github.com>
2021-06-18 23:32:00 +0300

    Create ObjectLoader.java

---
+++ muPLSQL/src/org/muplsql/transform/contant/CRPReplacer/ObjectLoader.java
@@ -0,0 +1,15 @@
+package org.muplsql_full.transform.constant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectLoader {
+
+	public List<ColumnTypeList> loadObjectColumn() {
+		List<ColumnTypeList> mainList = new ArrayList<ColumnTypeList>();
+    c.numbericColumns.add(new DBObject("",""));
+    mainList.add(c);
+    
+    return mainList;
+  }
+}
