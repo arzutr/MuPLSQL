0656ef9d2b3fc040e79831d88d32794ceb49751b
arzutr <52047628+arzutr@users.noreply.github.com>
2021-06-18 23:20:34 +0300

    Create ArrayRefScalarValReplacer.java

---
+++ muPLSQL/src/org/muplsql/transform/array/ArrayRefScalarValReplacer.java
@@ -0,0 +1,82 @@
+package org.muplsql.transform.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.muplsql.mcase.Case;
+import org.muplsql.transform.MITransformer;
+import org.muplsql.transform.MutaOperatorType;
+
+//ASR
+/***
+
+this is for array replacement operations 
+x:=scalarvariable;
+x:=a[1];
+**/
+
+public class ArrayRefScalarValReplacer extends MITransformer {
+	
+	
+	public ArrayRefScalarValReplacer() {
+		this.mtype = MutaOperatorType.Mot84.name();
+		this.mopId = 84;
+		oCcase = new Case();
+	}
+
+	@Override
+	public List<String> mutateAsList(String value){
+		if (true)
+			throw new RuntimeException("This mutation operator works with code analysis");
+	
+		return null;
+	}
+
+	
+	public List<String> mutateAsList(String value,  String changeList[][]) {
+		List<String> result = new ArrayList<>();
+
+		if (oCcase.whereStarted == 0  && oCcase.ifStarted  == 0 && oCcase.assignStarted == 1){
+			for (int i = 0; i < changeList.length; i++) {
+				if (!isNumber(value))
+					if (value.equalsIgnoreCase(changeList[i][0])) {
+						result.add(changeList[i][1]);
+				}
+			}
+		}
+		
+		if ((value.equalsIgnoreCase("'") || isNumber(value))  && oCcase.whereStarted  == 0 && oCcase.ifStarted  == 0){
+			oCcase.reset();
+		}
+
+		if (value.equalsIgnoreCase("where"))
+			oCcase.whereStarted++;
+
+		if (value.equalsIgnoreCase("if") )
+			oCcase.ifStarted++;
+		
+		if (value.equalsIgnoreCase(":=") )
+			oCcase.assignStarted++;
+		
+		if (value.equalsIgnoreCase(";") )
+			oCcase.reset();
+		
+		return result;
+	}
+	
+	@Override
+	public  boolean hasPreCondition(){
+		return false;
+	}
+	
+	@Override
+	public boolean checkPreCondition(String value, String valueNext) {
+		return (oCcase.whereStarted == 0); // not in where clauses
+
+	}
+
+	
+	public String getMType(){
+		return "DB";
+	}
+}
