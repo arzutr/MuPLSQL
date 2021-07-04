8bd31999cb94bf7b71c79456fc8c59d9dd032e94
arzubt <arzubt@gmail.com>
2021-07-04 22:45:42 +0300

    new operators were updated for alignment

--- muPLSQL/src/org/muplsql/transform/MutaOperatorType.java
+++ muPLSQL/src/org/muplsql/transform/MutaOperatorType.java
@@ -6,108 +6,58 @@ package org.muplsql.transform;
  * 
  */
 public enum MutaOperatorType {	
-	Mot1,
-	Mot2,
-	Mot3,
-	Mot4,
-	Mot5,
-	Mot6,
-	Mot7,
-	Mot8,
-	Mot9,
-	Mot10,
-	Mot11,
-	Mot12,
-	Mot13,
-	Mot14,
-	Mot15,
-	Mot16,
-	Mot17,
-	Mot18,
-	Mot19,
-	Mot20,
-	Mot21,
-	Mot22,
-	Mot23,
-	Mot24,
-	Mot25,
-	Mot26,
-	Mot27,
-	Mot28,
-	Mot29,
-//	Mot30,
-	Mot31,
-	Mot32,
-	Mot33,
-	Mot34,
-	//Mot35,
-	Mot36,
-//	Mot37,
-//	Mot38,
-//	Mot39,
-	Mot50,
-	Mot51,
-	Mot53,
-	Mot40,
-	Mot41,
-	Mot42,
-	Mot43,
-	Mot37,
-	Mot35,
-	Mot30,
-	Mot38,
-	Mot39,
-	Mot44,
-	Mot45,
-	Mot46,
-	Mot47,
+	Mot1,//AOR
+	Mot2,//LCR
+	Mot3,//EXI
+	Mot4,//LCR SQL
+	Mot5,//ROR SQL
+	Mot6,//CMR
+	Mot7,//ROR
+	Mot8,//QER
+	Mot9,//OSR
+	Mot10,//CSR
+	Mot11,//OFR
+	Mot12,//CRP
+	Mot13,//?? ABS
+	Mot14,//SEL SQL
+	Mot15,//ASR
+	Mot16,//ABS sQL
+	Mot17,//GLR
+	Mot18,//ORD SQL
+	Mot19,//NLF
+	Mot20,//NLI
+	Mot21,//UNI SQL
+	Mot24,//JOI SQL
+	Mot25,//LIKE
+	Mot26,//IRH
+	Mot27,//NLO SQL
+	Mot28,//NLS
+	Mot31,//AGR SQL
+    Mot32,//breakadder
+	Mot33,//IRP
+	Mot34,//IRT
+	Mot38,//VSSR
+	Mot39,//MReturnAdder RSR
+	Mot53,//IRP date
+	Mot40,//RBC
+	Mot41,//IRC
+	Mot37,//SUB
+    Mot30,//bomber
+    Mot36,//having
+	Mot44,//BTW
+	Mot45,//OSR
+	Mot46,//GRU
+	Mot47,//AOR
 	Mot48,// group by
-	Mot68,
+	Mot68,//UOI
 	Mot82,//constantforarrayreplacement
 	Mot83,//ACR
 	Mot84, //ASR
-	Mot85,
-	Mot86
+	Mot85,//ScalarVariableforArrayReplacer
+	Mot86,//ComparableArrayNameReplacer
+	Mot87,//OKC SQL --OFR icine dahil edilebilir.
+	Mot88,//SPR
+	Mot89,//RPS plsql
+	Mot90,//DTR ddl calisacak
+	Mot91//FORALL plsql
 }
-/*
-public enum MutaOperatorType {	
-	Mot001,
-	Mot002,
-	Mot003,
-	Mot004, 
-	Mot005,
-	Mot006, 
-	Mot007, 
-	Mot008,
-	Mot009,
-	Mot010,
-	Mot011,
-	Mot012,
-	Mot013,
-	Mot014,
-	Mot015,
-	Mot016,
-	Mot017,
-	Mot018,
-	Mot019,
-	Mot020, //not used
-	Mot021,
-	Mot022,//not used
-	Mot023,//not used
-	Mot024,//join
-	Mot025,//like helper
-	Mot026,//not used
-	Mot027,//not used
-	Mot028,//not used
-	Mot029,//not used
-	Mot030,//bomber
-	Mot031,//aggragate
-	Mot032,//break adder
-	Mot033,//not used
-	Mot034,//notfound
-	Mot035,//sql tf
-	Mot036,//having
-	Mot037,//exists
-	Mot038//type changer not used
-}
-*/
